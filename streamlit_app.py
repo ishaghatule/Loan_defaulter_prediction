182e8be5f10e4a65d1b340237bd0574c858dc4da
Isha Ghatule <67576061+ishaghatule@users.noreply.github.com>
2025-06-04 18:54:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/193E_Og5JlOvRGnVspMd_H1cgTbpv9tkI
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import joblib
